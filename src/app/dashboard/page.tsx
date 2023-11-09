a34fa0ad6eb6eed1271241e1bb9889347bbe1cd0
MartinStokholmWinTerm <msldk6a@gmail.com>
2023-11-09 16:58:03 +0100

    more personaltrainer stuff

--- src/app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Dashboard() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between"></main>
-  );
-}
---
+++ src/app/dashboard/personaltrainer/reports/page.tsx
@@ -0,0 +1,26 @@
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/app/api/auth/[...nextauth]/options";
+import WorkoutProgramList from "@/components/WorkoutProgramList";
+
+export default async function ReportsPage() {
+  const session = await getServerSession(authOptions);
+  const token = session?.user?.token;
+  const url =
+      "https://afefitness2023.azurewebsites.net/api/WorkoutPrograms/trainer/";
+
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const data = await response.json();
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between">
+        <div className="w-full p-4">
+          <h2 className="text-4xl pb-8">Reports</h2>
+        </div>
+      </main>
+  );
+}
