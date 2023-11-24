664c9aebb48322a24da1c5a1a7aa18fdc3983c85
MartinStokholmWinTerm <msldk6a@gmail.com>
2023-11-24 11:21:38 +0100

    fixes nav

--- src/app/dashboard/client/layout.tsx
+++ src/app/dashboard/client/layout.tsx
@@ -5,11 +5,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <main className="flex">
       <div className="flex justify-start ">
         <div className="flex flex-col justify-start bg-green-100 min-w-[200px]">
-          <DashboardNav
-            route="workoutprograms"
-            title="Programs"
-            bgColor="bg-amber-500"
-          />
+          <DashboardNav route="/" title="Programs" bgColor="bg-amber-500" />
         </div>
       </div>
       {children}
